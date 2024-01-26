fe7fd8bee43b98cd971010325347dd2ad1bc90c4
Artem Ivannikov <xthemey@yandex.ru>
2024-01-26 21:45:34 +0300

    Delete archive directory

--- archive/client/src/components/AsideBlock/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./AsideBlock.module.scss";
-
-export default function AsideBlcok() {
-  return <aside className={styles.aside}>ASIDEfdfdfdfdfdf</aside>;
-}
