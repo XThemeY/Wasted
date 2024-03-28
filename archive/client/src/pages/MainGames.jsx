c1d2610d042343fe87a5c8f0240dd3ab3640c00d
XThemeY <xthemey@yandex.ru>
2024-03-28 03:09:35 +0300

    fix

--- archive/client/src/pages/MainGames.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MainGames() {
-  return (
-    <main className="games">
-      <div className="container games__container"></div>
-    </main>
-  );
-}
